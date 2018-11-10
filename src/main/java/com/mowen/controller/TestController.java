741a1a41821a8c215aacdc93ea26280c760412d0
aixuedeyuoO <847102369@qq.com>
2018-11-10 22:36:53 +0800

    [Description]适配jdk11及assembly打包

--- src/main/java/com/mowen/controller/TestController.java
+++ src/main/java/com/mowen/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.mowen.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,10 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Jun Mowen
  * @date 2018/11/4
  */
+@Slf4j
 @RestController
 public class TestController {
     @RequestMapping("/hello")
     public String hello(){
+        log.info("test hello world!");
         return "Hello World";
     }
 }
